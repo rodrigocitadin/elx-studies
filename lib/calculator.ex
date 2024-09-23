5ff9afea7c43f4821671b7107e02360b24aaf135
rodrigocitadin <citadinrodrigo@gmail.com>
2024-09-23 19:58:53 -0300

    simple calculator

---
+++ lib/calculator.ex
@@ -0,0 +1,45 @@
+defmodule Calculator do
+  def start do
+    spawn(fn -> loop(0) end)
+  end
+
+  defp loop(current_value) do
+    new_value =
+      receive do
+        {:value, client_id} ->
+          send(client_id, {:response, current_value})
+          current_value
+
+        {:add, value} ->
+          current_value + value
+
+        {:sub, value} ->
+          current_value - value
+
+        {:mult, value} ->
+          current_value * value
+
+        {:div, value} ->
+          div(current_value, value)
+
+        invalid_request ->
+          IO.puts("Invalid Request #{inspect(invalid_request)}")
+          current_value
+      end
+
+    loop(new_value)
+  end
+
+  def value(server_id) do
+    send(server_id, {:value, self()})
+
+    receive do
+      {:response, value} -> value
+    end
+  end
+
+  def addition(server_id, value), do: send(server_id, {:add, value})
+  def subtraction(server_id, value), do: send(server_id, {:sub, value})
+  def multiply(server_id, value), do: send(server_id, {:mult, value})
+  def divide(server_id, value), do: send(server_id, {:div, value})
+end
